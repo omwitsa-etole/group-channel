aaa98a3c823aecbcb75625ee9d27603db959ea12
bradone-etole <omwitsabradone@gmail.com>
2022-04-09 11:11:44 +0300

    update

--- channel/settings.py
+++ channel/settings.py
@@ -124,10 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = 'data/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "data/media")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
