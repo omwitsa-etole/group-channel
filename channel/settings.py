efa5bf237c94b1f08459e8c4563fbb3802186980
bradone-etole <omwitsabradone@gmail.com>
2022-04-10 21:12:41 +0300

    update 1.2

--- channel/settings.py
+++ channel/settings.py
@@ -127,23 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-USE_S3 = os.environ.get('USE_S3') == 'True'
+USE_S3 = os.environ.get('USE_S3', 'True') == 'True'
 
 if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'channel.storage_backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'channel.storage_backends.StaticStorage'
     # s3 public media settings
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = 'channel.storage_backends.PublicMediaStorage'
